38f2318c926976837fb9feddcd14ff46056d2384
Youps <emaildeyouness@gmail.com>
2026-04-24 16:55:39 +0200

    Début du développement des controlleurs

--- app/Controllers/Formation.php
+++ app/Controllers/Formation.php
@@ -3,9 +3,21 @@
 namespace App\Controllers;
 
 class Formation extends BaseController {
-
-    function index() :void {
-        //return view('formation_index');
+    /**
+     * Retourne a la vue toutes les données de mes formations trié par id croissant
+     *
+     * @return string un tableau de formation contenant toutes les données de mes formations
+     */
+    function index() :string {
+        //je crée une nouvelle instance de mon modèle
+        $formationModel = new FormationModel();
+        //je demande toutes les données de mon modèle
+        $formations = $formationModel->findAll();
+        /*Je stocke dans un tableau associatif tout les éléments
+        trouvés dans ma bdd*/
+        $data['listeFormation'] = $formations;
+        //y'a plus qu'a tout régurgiter a la view
+        return view('formation_index', $data);
     }
 
     function search() :void {
