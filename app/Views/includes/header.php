52d20359667729fef93a14caf5c3bc77a89c14f0
Youps <emaildeyouness@gmail.com>
2026-05-31 19:27:28 +0200

    J'ai juste pré fait mis les fichiers includes

---
+++ app/Views/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>En Formation!</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <!--References vers mes autres pages-->
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li></li> 
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
